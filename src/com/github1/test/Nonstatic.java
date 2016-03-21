9477c014153ae923b4e347cbe223a555f22ac675
xavier.joseph <xavier.joseph@D402.blr.velankani.com>
2016-03-21 10:56:47 +0530

    Githubtest1

---
+++ src/com/github1/test/Nonstatic.java
@@ -0,0 +1,18 @@
+package com.github1.test;
+
+public class Nonstatic 
+{ 
+   int a=9;
+   void velankani()
+   {
+	   System.out.println("hi");
+   }
+   
+   public static void main(String[] args) 
+   
+   {
+	    Nonstatic obj = new Nonstatic();
+	    obj.velankani();
+	    System.out.println(obj.a);
+   }
+}
